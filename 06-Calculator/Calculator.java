9421ae0851ec66024241cae242114972ad816ce4
Ansari Bilal <m.bilal8492@gmail.com>
2019-12-30 21:07:32 +0530

    add calculator program

---
+++ 06-Calculator/Calculator.java
@@ -0,0 +1,48 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("please enter your choice");
+        System.out.println("1.Addition");
+        System.out.println("2.Substraction");
+        System.out.println("3.Division");
+        System.out.println("4.Multiplicatio");
+        String choice = System.console().readLine();
+
+        if (choice.equals("1")) {
+            System.out.println("please enter two number for addition");
+            String FirstNumber = System.console().readLine();
+            String SecondNumber = System.console().readLine();
+
+            int sum = Integer.parseInt(FirstNumber) + Integer.parseInt(SecondNumber);
+            System.out.println(" the addition of " + FirstNumber + " and " + SecondNumber + " is " + sum);
+
+        } else if (choice.equals("2")) {
+            System.out.println("please enter two number for substraction");
+            String FirstNumber = System.console().readLine();
+            String SecondNumber = System.console().readLine();
+
+            int sub = Integer.parseInt(FirstNumber) - Integer.parseInt(SecondNumber);
+            System.out.println(" the substraction of " + FirstNumber + " and " + SecondNumber + " is " + sub);
+
+        } else if (choice.equals("3")) {
+            System.out.println("please enter two number for division");
+            String FirstNumber = System.console().readLine();
+            String SecondNumber = System.console().readLine();
+
+            int div = Integer.parseInt(FirstNumber) / Integer.parseInt(SecondNumber);
+            System.out.println(" the division of " + FirstNumber + " and " + SecondNumber + " is " + div);
+
+        } else if (choice.equals("4")) {
+            System.out.println("please enter two number or multiplication");
+            String FirstNumber = System.console().readLine();
+            String SecondNumber = System.console().readLine();
+
+            int mul = Integer.parseInt(FirstNumber) * Integer.parseInt(SecondNumber);
+            System.out.println(" the multiplication of " + FirstNumber + " and " + SecondNumber + " is " + mul);
+
+        } else {
+            System.out.println("Wrong choice");
+        }
+
+    }
+}
