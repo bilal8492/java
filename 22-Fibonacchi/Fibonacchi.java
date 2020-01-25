7d95b340a540a7110d8c61c7484b1ff6ae03c01c
Ansari Bilal <m.bilal8492@gmail.com>
2020-01-25 23:19:33 +0530

    understand

--- 22-Fibonacchi/Fibonacchi.java
+++ 22-Fibonacchi/Fibonacchi.java
@@ -5,15 +5,17 @@ public class Fibonacchi {
 
     public static void main(String[] args) {
         System.out.println("enter a number to find fibonacchi series");
-        int Number=Integer.parseInt(System.console().readLine());
-        int x=0;
-        int y=1;
-    
-        for(int i=1; i<=Number; i++){
-            int z=x+y;
-            x=y;
-            y=z;
-            System.out.println(z);
+        int Number = Integer.parseInt(System.console().readLine());
+        int x = 0;
+        int y = 1;
+        int z = 0;
+        System.out.print(x + " " + y + " ");
+        for (int i = 2; i < Number; i++) {
+            z = x + y;
+            x = y;
+            y = z;
+            System.out.print(z + " ");
         }
+
     }
 }
--- 23-Prime-Number/PrimeNumber.java
+++ 23-Prime-Number/PrimeNumber.java
@@ -5,19 +5,19 @@ public class PrimeNumber {
 
     public static void main(String[] args) {
         System.out.println("Write number to find prime number or not");
-        int Number=Integer.parseInt(System.console().readLine());
-        int count=0;
-        for(int i=1;i<=Number;i++){
-            if (Number%i==0) {
-                count++;    
+        int Number = Integer.parseInt(System.console().readLine());
+        boolean IsPrime=true;
+        for (int i = 2; i < Number; i++) {
+            if (Number % i == 0) {
+                IsPrime=false;
+                break;
             }
-            
+
         }
-        if (count==2){
-            System.out.println("Number is prime");
-        }
-        else {
+        if (IsPrime == false) {
             System.out.println("Number is not prime");
+        } else {
+            System.out.println("Number is  prime");
         }
     }
 }
